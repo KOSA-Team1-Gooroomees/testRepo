17fa26ae4c0703faa10b138b094860f0a4f96531
minho2618 <minho2618@naver.com>
2026-04-29 17:04:47 +0900

    Feature#2 - 이슈 테스트

--- test-project/src/main/java/com/example/test_project/controller/TestController2.java
+++ test-project/src/main/java/com/example/test_project/controller/TestController2.java
@@ -1,4 +1,13 @@
 package com.example.test_project.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController2 {
+    @GetMapping("/test2")
+    public String hello() {
+        return "Hello World";
+    }
 }
